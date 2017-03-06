44254315874276f6c9e2ac327a49d81174dce316
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-03-06 17:27:11 +0100

    Add hello world video playing using video.js

--- page.js
+++ page.js
@@ -1,8 +1,11 @@
-var $ = require('jquery');
+const $ = require('jquery');
+const vjs = require('video.js');
 
 $(document).ready(() => {
     console.log('hello world');
 
     const text = document.getElementById('text');
     text.innerHTML = 'hello';
+
+    vjs('vid');
 });
